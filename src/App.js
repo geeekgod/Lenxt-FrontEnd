848a4c7a29376eeba43df66709689a823a4305cd
Rishabh Singh <ris8645208@gmail.com>
2021-11-19 22:05:34 +0530

    style: update auth pages

--- src/App.js
+++ src/App.js
@@ -1,135 +1,19 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import {Login} from '@mui/icons-material';
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import signInImg from "./assets/imgs/signin.png";
-import { FormControlLabel, FormGroup, Checkbox } from "@mui/material";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
-export default function App() {
-  const [showPassword, setShowPassword] = React.useState(false);
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [disableLogin, setDisableLogin] = React.useState(false);
-
-  React.useEffect(() => {
-    if (
-      email !== "" &&
-      email !== " " &&
-      password !== "" &&
-      password !== " " &&
-      password.length > 7 &&
-      email.indexOf("@") != -1 &&
-      email.indexOf(".") != -1
-    ) {
-      setDisableLogin(false);
-    } else {
-      setDisableLogin(true);
-    }
-  }, [email, password]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({
-      email: email,
-      password: password,
-    });
-  };
-
+const App = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item md={6}>
-        <img src={signInImg} style={{ width: "100%" }} />
-      </Grid>
-      <Grid item md={6} sx={{ justifyContent: "center", margin: "auto" }}>
-        <Container component="main" maxWidth="xs">
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Welcome Back to <b style={{fontWeight: 600}}>LENXT</b>
-            </Typography>
-            <Typography component="h2" variant="h5">
-              Sign to your account
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1, padding: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                disableElevation
-                disabled={disableLogin}
-                endIcon={<Login/>}
-              >
-                Sign In
-              </Button>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={showPassword}
-                      onChange={() => {
-                        setShowPassword(!showPassword);
-                      }}
-                    />
-                  }
-                  label="Show Password"
-                />
-              </FormGroup>
-              <Grid container>
-                <Grid item xs></Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Container>
-      </Grid>
-    </Grid>
+    <Router>
+      <Routes>
+        <Route path="auth">
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
+
+export default App;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { LockOpen } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import signUpImg from "../../assets/imgs/signup.png";
+import { FormControlLabel, FormGroup, Checkbox, Alert, CircularProgress } from "@mui/material";
+import { Link as RLink } from "react-router-dom"
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [cnfPassword, setCnfPassword] = useState("");
+  const [disableLogin, setDisableLogin] = useState(false);
+  const [errPswd, setErrPswd] = useState(false);
+  const [submit, setSubmit] = useState(false);
+
+  useEffect(() => {
+    if (
+      email !== "" &&
+      email !== " " &&
+      name !== "" &&
+      name !== " " &&
+      name.length > 7 &&
+      password !== "" &&
+      password !== " " &&
+      password.length > 7 &&
+      cnfPassword !== "" &&
+      cnfPassword !== " " &&
+      cnfPassword.length > 7 &&
+      email.indexOf("@") != -1 &&
+      email.indexOf(".") != -1
+    ) {
+      setDisableLogin(false);
+    } else {
+      setDisableLogin(true);
+    }
+  }, [email, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (cnfPassword === password) {
+      setSubmit(true);
+      setTimeout(() => {
+        console.log({
+          email: email,
+          password: password,
+        });
+        setSubmit(false);
+      }, 5000);
+    }
+    else {
+      setErrPswd(true);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Sign Up | Lenxt";
+  }, []);
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item md={6} sx={{ justifyContent: "center", margin: "auto" }}>
+          <Container component="main" maxWidth="xs">
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginTop: 10,
+                marginBottom: 10
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Welcome to <b style={{ fontWeight: 600 }}>LENXT</b>
+              </Typography>
+              <Typography component="h2" variant="h5">
+                Create an Account
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1, padding: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Full Name"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    if (errPswd === true) {
+                      setErrPswd(false)
+                    }
+                  }}
+                  error={errPswd}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="cnfPassword"
+                  label="Confirm Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={cnfPassword}
+                  onChange={(e) => {
+                    setCnfPassword(e.target.value);
+                    if (errPswd === true) {
+                      setErrPswd(false)
+                    }
+                  }}
+                  error={errPswd}
+                />
+                {errPswd ? (<Alert severity="error">Both password should be same</Alert>) : null}
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disableElevation
+                  disabled={submit ? false : disableLogin}
+                  endIcon={submit ? null : <LockOpen />}
+                >
+                  {submit ? (<CircularProgress />) : "Register"}
+                </Button>
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={showPassword}
+                        onChange={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    }
+                    label="Show Password"
+                  />
+                </FormGroup>
+                <Grid container>
+                  <Grid item xs></Grid>
+                  <Grid item>
+                    <RLink to="/auth/signin">
+                      <Link variant="body2">
+                        {"Already have an account? Sign in"}
+                      </Link>
+                    </RLink>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </Grid>
+        <Grid item md={6}>
+          <img src={signUpImg} style={{ width: "100%" }} />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default SignUp;
