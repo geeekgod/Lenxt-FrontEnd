7cf4da1e52ffff720cd3cf33869abab8080f918a
Rishabh Singh <ris8645208@gmail.com>
2021-11-20 21:39:40 +0530

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
-const Home = () => {
-  return(
-    <div>
-        <h1>Lenxt Landing</h1>
-        <Link to="/auth/signin">Sign in</Link>
-        <br/>
-        <Link to="/auth/signup">Sign Up</Link>
-    </div>
-  )
-}
 
 const App = () => {
   return (
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,103 @@
+import { ArrowForwardIos } from "@mui/icons-material";
+import {
+  Box,
+  Grid,
+  useTheme,
+  Typography,
+  Container,
+  Button,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import landingImg from "../../assets/imgs/landing.png";
+
+const Home = () => {
+  const theme = useTheme();
+
+  useEffect(() => {
+    document.title = "Lenxt";
+  }, []);
+  return (
+    <>
+      <Box sx={{ background: "#f7f9fe" }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ minHeight: "100vh", justifyContent: "center" }}
+        >
+          <Grid
+            item
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Container maxWidth="xs">
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                component="div"
+                sx={{ fontWeight: 600, color: "#3867A8" }}
+              >
+                Video Chat with smart lens
+              </Typography>
+              <Typography variant="h4" component="h5" gutterBottom>
+                Chat, Call securely & discover the world with{" "}
+                <b
+                  style={{ fontWeight: 600, color: theme.palette.primary.main }}
+                >
+                  Lenxt
+                </b>
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
+                eligendi minima non et consequuntur repellendus. Aperiam
+                consectetur amet nemo maxime molestias rem praesentium.
+              </Typography>
+              <Link  to="/auth/signup">
+                <Button
+                  variant="contained"
+                  sx={[
+                    {
+                      paddingTop: 1.4,
+                      paddingBottom: 1.4,
+                      marginTop: 3.4,
+                      fontSize: 14.7,
+                      paddingLeft: 3,
+                      paddingRight: 3,
+                    },
+                    () => ({
+                      "&:hover": {
+                        transform: `translateY(-10px)`,
+                      },
+                    }),
+                  ]}
+                  className="button-custom"
+                  disableElevation
+                  endIcon={<ArrowForwardIos />}
+                >
+                  Get Started
+                </Button>
+              </Link>
+            </Container>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={landingImg} style={{ width: "90%" }} />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
